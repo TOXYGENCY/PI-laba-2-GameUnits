1e0612562d552f35a01305bb89aea713ecc825ed
TOXYGENCY <mailforsomethingelse@gmail.com>
2024-04-06 18:10:21 +0300

    Добавил базовый абстрактный класс Unit.cs с полями и методами

---
+++ PI-laba-2-GameUnits/Unit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace PI_laba_2_GameUnits
+{
+    public abstract class Unit
+    {
+        public double HealthPoints { get; set; }
+        public int DistanceToExplosion { get; set; } 
+
+        protected virtual double CalculateDamage(double DistanceToExplosion, double ExplosionPower) // protected, потому что будет использоваться только внутри этого семейства классов
+        { // Сделаю общий метод по вычислению общего урона, ибо это значение нужно всем
+            var Damage = ExplosionPower / DistanceToExplosion;
+            Console.WriteLine($"CalculateDamage({DistanceToExplosion}, {ExplosionPower}) = {Damage} - базовый урон");
+            return Damage;
+        }
+
+        
+        public double CalculateHP(double DistanceToExplosion, double ExplosionPower)
+        { // Общий рассчет ХП
+            var newHP = HealthPoints - CalculateDamage(DistanceToExplosion, ExplosionPower);
+            Console.WriteLine($"CalculateHP({DistanceToExplosion}, {ExplosionPower}) = {newHP} - здоровье теперь");
+            return newHP;
+        }
+
+    }
+}

016680188822f9c9be8cf2fb0f8447e233bbb2c5
TOXYGENCY <mailforsomethingelse@gmail.com>
2024-04-06 16:14:01 +0300

    Add project files.

---
+++ PI-laba-2-GameUnits/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PI_laba_2_GameUnits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

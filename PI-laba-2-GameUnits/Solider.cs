fc25d4efd82d8249b9535b81edd08878ee42b16d
TOXYGENCY <mailforsomethingelse@gmail.com>
2024-04-06 19:10:44 +0300

    Добавил Tank и Solider. Переопределил метод в Tank

---
+++ PI-laba-2-GameUnits/Solider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_laba_2_GameUnits
+{
+    public class Solider : Unit
+    {
+        // и чё? я же все прописал уже в Unit, получается...
+    }
+}
---
+++ PI-laba-2-GameUnits/Tank.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_laba_2_GameUnits
+{
+    public class Tank : Unit
+    {
+        public byte Armor { get; set; } // Броня будет от 0 до 100 потому что будут махинации с процентами
+        protected override double CalculateDamage(double DistanceToExplosion, double ExplosionPower)
+        { // Переопределим метод вычисления урона для конкретно танка
+            var Damage = ExplosionPower / DistanceToExplosion;
+            Damage -= Damage * (Armor / 100);
+            return Damage;
+        }
+    }
+}
